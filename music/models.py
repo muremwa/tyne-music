10349b9661b048317ccf8d63ead06c7c2d3651c9
Muremwa <danmburu254@gmail.com>
2020-01-11 14:36:03 +0300

    unused imports

--- music/models.py
+++ music/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.utils.text import slugify
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Artist(models.Model):
