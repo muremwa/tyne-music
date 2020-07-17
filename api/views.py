8b5933ad3ac862f2e8ef16f2c2a93304801c6f6b
Muremwa <danmburu254@gmail.com>
2020-07-18 01:41:17 +0300

    added album filters

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import get_object_or_404
+import datetime
+import re
+
 from rest_framework import views, generics
 
 from music.models import Album, Artist, Genre, Category
@@ -56,27 +58,78 @@ class FetchCategories(views.APIView):
 
 
 class FetchAlbums(views.APIView):
-    """ Fetch albums  """
+    """
+    fetch published albums,
+    add artist='name' in query to filter by artists
+
+    for time period use
+    1. before='YYYY-MM-DD' -> before a certain date
+    2. after='YYYY-MM-DD' -> after a certain date
+    3. on='YYYY-MM-DD' -> on a certain date
+
+    You can combine 1 and 2 to filter albums between two dates
+    ?after='YYYY-MM-DD'&'before='YYYY-MM-DD'
+        the dates should not conflict e.g. before 2017 but after 2019
+
+    """
+
     @staticmethod
-    def get(request):
-        """
-        fetch published albums,
-        add artist='name' in query to filter by artists
+    def is_time_format_valid(current_date: str) -> bool:
+        valid = False
 
-        for time period use
-        1. before='YYYY-MM?-DD?' ->
-        2. after='YYYY-MM?-DD?'
-        3. on='YYYY-MM-DD'
+        if re.search('\d{4}-\d{2}-\d{2}', current_date):
+            date = [int(char) for char in current_date.split('-')]
 
-        You can combine 1 and to get the above to
+            if len(date) == 3:
+                try:
+                    datetime.datetime(date[0], date[1], date[2])
+                    valid = True
+                except ValueError:
+                    pass
 
-        """
+        return valid
+
+    def time_filter(self, q_set, params):
+        """ date_of_release time filters """
+
+        for key, item in params.items():
+            if self.is_time_format_valid(item):
+                if key == 'before':
+                    q_set = q_set.filter(date_of_release__lte=item)
+
+                elif key == 'after':
+                    q_set = q_set.filter(date_of_release__gte=item)
+
+                elif key == 'on':
+                    q_set = q_set.filter(date_of_release=item)
+
+        return q_set
+
+    def get(self, request):
+        albums = Album.objects.filter(published=True)
+
+        # filter by artist
+        artist = request.GET.get('artist', None)
+        if artist:
+            albums = albums.filter(artist__name__icontains=artist)
+
+        # time filters
+        time_filters = {
+            'before': request.GET.get('before', ''),
+            'after': request.GET.get('after', ''),
+            'on': request.GET.get('on', ''),
+        }
+
+        if any(time_filters.values()):
+            albums = self.time_filter(albums, time_filters)
+
         albums_data = AlbumSerializer(
-            instance=Album.objects.filter(published=True),
+            instance=albums,
             many=True,
             no_songs=True,
             read_only=True
         ).data
+
         return views.Response({
             'albums': albums_data
         })
